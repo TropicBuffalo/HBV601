8f4c1f46b3c112f8833344ab4451fbfa7ad0b849
peturinn <petur_t@hotmail.com>
2018-02-11 16:47:25 +0000

    drawers + wagon summary

---
+++ app/src/main/java/com/example/notandi/hospitalwagons/Drawers.java
@@ -0,0 +1,20 @@
+package com.example.notandi.hospitalwagons;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Petur on 11.2.2018.
+ */
+
+public class Drawers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawers);
+    }
+
+    
+
+}
---
+++ app/src/main/java/com/example/notandi/hospitalwagons/WagonSummary.java
@@ -0,0 +1,17 @@
+package com.example.notandi.hospitalwagons;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Petur on 11.2.2018.
+ */
+
+public class WagonSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wagon_summary);
+    }
+}
