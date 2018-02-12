66dc5a40cd092c12c0a67e97770b1fbfe2f222da
peturinn <petur_t@hotmail.com>
2018-02-12 20:30:04 +0000

    tenging, placeholder a einu

--- app/src/main/java/com/example/notandi/hospitalwagons/Drawers.java
+++ app/src/main/java/com/example/notandi/hospitalwagons/Drawers.java
@@ -1,20 +1,41 @@
 package com.example.notandi.hospitalwagons;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Petur on 11.2.2018.
  */
 
 public class Drawers extends AppCompatActivity {
+    private Button afram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawers);
+
+        afram = (Button) findViewById(R.id.afram);
+        afram.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openWagonSummary();
+            }
+        });
+
+
     }
 
-    
+    public void openWagonSummary(){// takes the user to wagon page when the button is pressed
+        Intent intent = new Intent(this,WagonSummary.class);
+        startActivity(intent);
+    }
+
+
+    });
 
 }
--- app/src/main/java/com/example/notandi/hospitalwagons/WagonSummary.java
+++ app/src/main/java/com/example/notandi/hospitalwagons/WagonSummary.java
@@ -1,7 +1,10 @@
 package com.example.notandi.hospitalwagons;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Petur on 11.2.2018.
@@ -9,9 +12,27 @@ import android.support.v7.app.AppCompatActivity;
 
 public class WagonSummary extends AppCompatActivity {
 
+    private Button naest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wagon_summary);
     }
+
+    /* naest = (Button) findViewById(R.id.neast);
+     naest.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            openplaceholder();
+        }
+    });
+    */
+    /*
+    public void openplaceholder(){// takes the user to wagon page when the button is pressed
+        Intent intent = new Intent(this,placeholder.class);
+        startActivity(intent);
+    }
+    */
 }
