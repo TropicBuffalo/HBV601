120a068c454ac1d940212b50c825866556653688
TropicBuffalo <fannzio@gmail.com>
2018-02-24 16:23:39 +0000

    Navigation bar Drawers
    Navigation bar Drawers

--- app/src/main/java/com/example/notandi/hospitalwagons/Drawers.java
+++ app/src/main/java/com/example/notandi/hospitalwagons/Drawers.java
@@ -2,40 +2,108 @@ package com.example.notandi.hospitalwagons;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-/**
- * Created by Petur on 11.2.2018.
- */
+import com.example.notandi.hospitalwagons.Fragment_Drawers.drawer1_Fragment;
+import com.example.notandi.hospitalwagons.Fragment_Drawers.drawer2_Fragment;
 
-public class Drawers extends AppCompatActivity {
-    private Button afram;
+public class Drawers extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawers);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        afram = (Button) findViewById(R.id.afram);
-        afram.setOnClickListener(new View.OnClickListener() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-            @Override
-            public void onClick(View view) {
-                openWagonSummary();
-            }
-        });
-
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    public void openWagonSummary(){// takes the user to wagon page when the button is pressed
-        Intent intent = new Intent(this,WagonSummary.class);
-        startActivity(intent);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawers, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        Fragment fragment = null;
+
+        int id = item.getItemId();
+        //Finna ID fyrir takkana í Drawers Navigation bar og tengja það við rétt fragment
+        if (id == R.id.nav_drawer1) {
+            fragment = new drawer1_Fragment();
+        } else if(id == R.id.nav_drawer2) {
+            fragment = new drawer2_Fragment();
+        }
+
+        else if (id == R.id.drawer_continue) {
+            Intent intent = new Intent(this, WagonSummary.class);
+            startActivity(intent);
+        } else if (id == R.id.drawer_comment) {
+            Intent intent = new Intent(this, CommentSummary.class);
+            startActivity(intent);
+        }
+
+        if(fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+
+            ft.replace(R.id.screen_area, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
---
+++ app/src/main/java/com/example/notandi/hospitalwagons/Fragment_Drawers/drawer2_Fragment.java
@@ -0,0 +1,28 @@
+package com.example.notandi.hospitalwagons.Fragment_Drawers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.notandi.hospitalwagons.R;
+
+/**
+ * Created by Notandi on 24.2.2018.
+ */
+
+public class drawer2_Fragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.drawer2_fragment, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
