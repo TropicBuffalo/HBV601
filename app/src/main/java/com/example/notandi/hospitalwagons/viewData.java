20f88546ecff1e987c5014cd99ae30c1ba50bf42
TropicBuffalo <fannzio@gmail.com>
2018-04-05 00:17:36 +0000

    Prototype retrieve data

--- app/src/main/java/com/example/notandi/hospitalwagons/viewData.java
+++ app/src/main/java/com/example/notandi/hospitalwagons/viewData.java
@@ -2,8 +2,26 @@ package com.example.notandi.hospitalwagons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class viewData extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private ListView mUserList;
+
+    private ArrayList<String> mUsernames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +30,41 @@ public class viewData extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Gagnaskoðun");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUserList = (ListView) findViewById(R.id.userlist);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, mUsernames);
+        mUserList.setAdapter(arrayAdapter);
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                mUsernames.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
